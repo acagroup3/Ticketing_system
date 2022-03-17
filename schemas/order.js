3711cdc3b124fa4b3becfec68e04dbb0dbbe25a4
Hasmik Grigoryan <hasmik.grigoryan9419@gmail.com>
2022-03-17 12:54:33 +0400

    Pushing mongoose schema files for project.

---
+++ schemas/order.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose')
+
+const orderSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: 'users' },
+  ordersList: [{
+    order: [{ type: Schema.Types.ObjectId, ref: 'tickets', _id: false }],
+    date: { type: Date, default: Date.now() },
+    _id: false
+  }],
+}, { collection: 'orders', strict: false })
+
+orderSchema.index({ userId: 1 }, { unique: true })
+
+module.exports = model('Order', orderSchema)
