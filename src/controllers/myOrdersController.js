09607115fb47696416834335799ee32972a3b40f
acagroup3 <hasmik.grigoryan9419@gmail.com>
2022-03-22 21:30:43 +0400

    Added validation middlewares for ticket data, implemented my-orders rout contollers, added some checks in createTicket function.

--- src/controllers/myOrdersController.js
+++ src/controllers/myOrdersController.js
@@ -1,30 +1,63 @@
-// const orders = require("../models/order");
+const Order = require("../models/order");
+const Ticket = require('../models/ticket');
+const User = require('../models/user');
 
+// Get all orders list for user
 async function getMyOrders(req, res) {
 	try{
-		res.send(JSON.stringify(req.headers));
+		const orders = await Order.findOne({ userId: req.headers['profile-id'] });
+		res.send(orders.ordersList);
 	}
 	catch(err){
 		console.log(err);
-		res.status(500).json({ error: 'something went wrong' });
+		res.status(500).json({ error: 'Something went wrong' });
 	}
 }
 
+// Get specified by id order details
 async function getMyOrder(req, res) {
 	try {
-		res.send(req.params);
+		const order = await Order.findOne({ userId: req.headers['profile-id']},
+										  {ordersList: {$elemMatch: {_id: req.params.orderId }}}); 
+		res.send(order.ordersList[0]);
+
 	} catch (err) {
 		console.log(err);
-		res.status(500).json({ error: 'something went wrong' });
+		res.status(500).json({ error: 'Something went wrong' });
 	}
 }
 
+// Cancel one ticket from order, if possible
 async function cancelTicket(req, res) {
 	try {
-		res.send(req.params);
+		const {ticketId} = req.params;
+		const userId = req.headers['profile-id'];
+		const {orderId} = req.params;
+		const ticket = await Ticket.findById(ticketId);
+		const user = await User.findById(userId);
+		
+		if(ticket.canCancel && ticket.cancelDate >= Date.now()){
+			// adding ticket quantity by one
+			await Ticket.findByIdAndUpdate(ticketId, {quantity: (ticket.quantity + 1)});
+			
+			// giving user's mony back
+			await User.findByIdAndUpdate(userId, {coins: (user.coins + ticket.price)});
+			
+			// deleting ticket from order, or deliting order if it contains only one ticket
+			// if(order.ordersList.length === 1){
+			// 		Order.deleteOne({_id: orderId}); ///????
+			// }
+			// TODO: query is not correct
+			const canceledOrder = await Order.findByIdAndUpdate(userId, {ordersList: {$pull: {_id: orderId }}});
+			console.log("updated orders: ", canceledOrder);
+			res.status(201).json({ cancelled: 'success' });
+		}
+				
+
+
 	} catch (err) {
 		console.log(err);
-		res.status(500).json({ error: 'something went wrong' });
+		res.status(500).json({ error: 'Something went wrong' });
 	}
 }
 
