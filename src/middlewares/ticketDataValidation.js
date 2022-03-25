b1335abbdd7936bac0aead41171e3e33339b5d1c
Hermine Gh <Hgh_Wteach@yahoo.com>
2022-03-25 20:17:03 +0400

    bug fix

--- src/middlewares/ticketDataValidation.js
+++ src/middlewares/ticketDataValidation.js
@@ -1,60 +1,86 @@
-const {body} = require("express-validator");
-const countryNames = require("../utils/countryNames");
+const { body } = require('express-validator');
+const countryNames = require('../utils/countryNames');
 
-const validateName = 
-	body("name").trim().isLength({ min: 3 })
-	.withMessage("Name is a mandatory field, it's shoul contain min 3 characters.");
+const validateName = body('name')
+	.trim()
+	.isLength({ min: 3 })
+	.withMessage(
+		"Name is a mandatory field, it's shoul contain min 3 characters."
+	);
 
-const validateDescription = 
-	body("description").trim().isLength({ min: 10 })
-	.withMessage("Description is a mandatory field, it's shoul contain min 10 characters.");
+const validateDescription = body('description')
+	.trim()
+	.isLength({ min: 10 })
+	.withMessage(
+		"Description is a mandatory field, it's shoul contain min 10 characters."
+	);
 
-const validateDate =
-    body("date").not().isEmpty()
-	.withMessage('Date is a mandatory field, it must be in yyyy-mm-dd format and in the future.')
-	.isDate().withMessage("Invalid date, should be in yyyy-mm-dd format and in the future")
-	.isAfter().withMessage("Date must be in future.");
+const validateDate = body('date')
+	.not()
+	.isEmpty()
+	.withMessage(
+		'Date is a mandatory field, it must be in yyyy-mm-dd format and in the future.'
+	)
+	.isDate()
+	.withMessage(
+		'Invalid date, should be in yyyy-mm-dd format and in the future'
+	)
+	.isAfter()
+	.withMessage('Date must be in future.');
 
-const validatePrice =
-    body("price").not().isEmpty().withMessage("Price is a mandatory field.")
-	.isFloat({gt: 0}).withMessage("Price must be positive number.");
+const validatePrice = body('price')
+	.not()
+	.isEmpty()
+	.withMessage('Price is a mandatory field.')
+	.isFloat({ gt: 0 })
+	.withMessage('Price must be positive number.');
 
-const validateQuantity = 
-	body("quantity").not().isEmpty().withMessage("Quantity is a mandatory field.")
-	.isNumeric().withMessage("Quantity must be numeric.")
-	.isInt({ gt: 0 }).withMessage("Quantity value should be positive integer number.");
+const validateQuantity = body('quantity')
+	.not()
+	.isEmpty()
+	.withMessage('Quantity is a mandatory field.')
+	.isNumeric()
+	.withMessage('Quantity must be numeric.')
+	.isInt({ gt: 0 })
+	.withMessage('Quantity value should be positive integer number.');
 
-const validatecanCancel = 
-	body("canCancel").isBoolean().withMessage("Cancelability must be boolean.")
+const validatecanCancel = body('canCancel')
+	.isBoolean()
+	.withMessage('Cancelability must be boolean.')
 	.default(false);
 
-const validateCancelDate = 
-	body("cancelDate").isDate().withMessage("Invalid date, should be in yyyy-mm-dd format and in the future")
-	.isAfter().withMessage("Invalid date")
-	// TODO: isBefore doesn't work as expected, it's always returns error
-	// .isBefore("date").withMessage("Cancel date must be before ticket date.");
+const validateCancelDate = body('cancelDate')
+	.isDate()
+	.withMessage(
+		'Invalid date, should be in yyyy-mm-dd format and in the future'
+	)
+	.isAfter()
+	.withMessage('Invalid date');
+// TODO: isBefore doesn't work as expected, it's always returns error
+// .isBefore("date").withMessage("Cancel date must be before ticket date.");
 
 const isBefore = (req, res, next) => {
-	if(req.body.date < req.body.cancelDate){
-		res.status(400).json({ error: "Cancel date must be before ticket date." });
-		next("Cancel date must be before ticket date.");
+	if (req.body.date < req.body.cancelDate) {
+		res.status(400).json({
+			error: 'Cancel date must be before ticket date.',
+		});
+		next('Cancel date must be before ticket date.');
 	}
 	next();
-}
+};
 
 const validateCountry = (req, res, next) => {
-	const countries = req.body.countries.split(", ");
+	const { countries } = req.body;
 	// eslint-disable-next-line no-restricted-syntax
 	for (const country of countries) {
-		if(!countryNames.includes(country)){
-			res.status(400).json({ error: "Country name is incorrect." });
-			next("Country name is incorrect.");
+		if (!countryNames.includes(country)) {
+			res.status(400).json({ error: 'Country name is incorrect.' });
+			next('Country name is incorrect.');
 		}
-	  }
+	}
 	req.body.countries = countries;
 	next();
-}
-	
+};
 
 module.exports = {
 	validateName,
@@ -65,5 +91,5 @@ module.exports = {
 	validatecanCancel,
 	validateCancelDate,
 	isBefore,
-	validateCountry
+	validateCountry,
 };
